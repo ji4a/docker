3a91296d6596b5819fad7a61c0788ec7f8a58567
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-18 13:46:44 +0100

    Create ccrypt.d

---
+++ ccrypt.d
@@ -0,0 +1,10 @@
+###### CCRYPT on Almalinux8 Base ######
+docker pull ji4a/ccrypt
+
+docker run -it -d --restart=always \
+  --name CCrypt \
+  ji4a/ccrypt bash
+
+ORRRRRR
+
+docker run -it --restart=always --name CCrypt ji4a/ccrypt bash
