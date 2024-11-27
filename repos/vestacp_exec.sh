597ba34794f0c9f2384d2ef7415245a2dbca3bc5
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-27 13:17:55 +0100

    Update vestacp_exec.sh

--- repos/vestacp_exec.sh
+++ repos/vestacp_exec.sh
@@ -1,10 +1,6 @@
 SERVER_IP=$(hostname -I | awk '{print $1}')
 DOMAIN_NAME=myworld.com
 
-/usr/local/vesta/bin/v-change-user-password admin Money22
-"echo 'FILEMANAGER_KEY=ILOVEREO' >> /usr/local/vesta/conf/vesta.conf"
-/usr/local/vesta/bin/v-add-web-domain admin ${SERVER_IP}.${DOMAIN_NAME}
-
 #### DELETE INDEX.HTML AND ROBOTS AND WGET INDEX.PHP ####
 rm -rf /home/admin/web/${SERVER_IP}.${DOMAIN_NAME}/public_html/robots.txt
 rm -rf /home/admin/web/${SERVER_IP}.${DOMAIN_NAME}/public_html/index.html
