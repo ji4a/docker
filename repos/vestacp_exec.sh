ebe83d0125944b2b3ee6a95f6d5f542e7962b99d
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-27 13:08:58 +0100

    Create vestacp_exec.sh

---
+++ repos/vestacp_exec.sh
@@ -0,0 +1,10 @@
+SERVER_IP=$(hostname -I | awk '{print $1}')
+DOMAIN_NAME=myworld.com
+
+/usr/local/vesta/bin/v-add-web-domain admin ${SERVER_IP}.${DOMAIN_NAME}
+
+#### DELETE INDEX.HTML AND ROBOTS AND WGET INDEX.PHP ####
+rm -rf /home/admin/web/${SERVER_IP}.${DOMAIN_NAME}/public_html/robots.txt
+rm -rf /home/admin/web/${SERVER_IP}.${DOMAIN_NAME}/public_html/index.html
+
+wget https://raw.githubusercontent.com/ji4a/php/main/v_index.php -O /home/admin/web/${SERVER_IP}.${DOMAIN_NAME}/public_html/index.php
